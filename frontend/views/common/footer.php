354b1bf280286221a7c049eb52e7fce747aff356
tetrostan <tetrostan@gmail.com>
2017-02-28 11:39:37 +0200

    Realization form in footer.

--- frontend/views/common/footer.php
+++ frontend/views/common/footer.php
@@ -5,27 +5,29 @@
             <div class="col-lg-3 col-sm-3">
                 <h4>Information</h4>
                 <ul class="row">
-                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="about.html" >About</a></li>
-                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="agents.html" >Agents</a></li>
-                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="blog.html" >Blog</a></li>
-                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="contact.html" >Contact</a></li>
+                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="about.html">About</a></li>
+                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="agents.html">Agents</a></li>
+                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="blog.html">Blog</a></li>
+                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="contact.html">Contact</a></li>
                 </ul>
             </div>
 
             <div class="col-lg-3 col-sm-3">
                 <h4>Newsletter</h4>
                 <p>Get notified about the latest properties in our marketplace.</p>
-                <form class="form-inline" role="form">
-                    <input type="text" placeholder="Enter Your email address" class="form-control">
-                    <button class="btn btn-success" type="button">Notify Me!</button></form>
+                <?php \yii\helpers\Html::beginForm('', 'post', ['class' => 'form-inline']) ?>
+                <?php echo \yii\helpers\Html::textInput('search', '', ['class' => 'form-control', 'placeholder' => 'Enter Your email address',
+                ]) ?>
+                <?php echo \yii\helpers\Html::submitButton('Notify Me!', ['class' => 'btn btn-success']) ?>
+                <?php \yii\helpers\Html::endForm() ?>
             </div>
 
             <div class="col-lg-3 col-sm-3">
                 <h4>Follow us</h4>
-                <a href="#"><img src="/images/facebook.png"  alt="facebook"></a>
-                <a href="#"><img src="/images/twitter.png"  alt="twitter"></a>
-                <a href="#"><img src="/images/linkedin.png"  alt="linkedin"></a>
-                <a href="#"><img src="/images/instagram.png"  alt="instagram"></a>
+                <a href="#"><img src="/images/facebook.png" alt="facebook"></a>
+                <a href="#"><img src="/images/twitter.png" alt="twitter"></a>
+                <a href="#"><img src="/images/linkedin.png" alt="linkedin"></a>
+                <a href="#"><img src="/images/instagram.png" alt="instagram"></a>
             </div>
 
             <div class="col-lg-3 col-sm-3">
@@ -36,5 +38,6 @@
                     <span class="glyphicon glyphicon-earphone"></span> (123) 456-7890</p>
             </div>
         </div>
-        <p class="copyright">Copyright 2013. All rights reserved.	</p>
-    </div></div>
+        <p class="copyright">Copyright 2013. All rights reserved. </p>
+    </div>
+</div>
