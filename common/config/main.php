ae057e8851332d5d02c733c3a7f712c9b495a0fa
tetrostan <tetrostan@gmail.com>
2017-03-08 17:40:30 +0200

    Change cache class to Base64Cache

--- common/config/main.php
+++ common/config/main.php
@@ -1,10 +1,19 @@
 <?php
 return [
+    'name' => 'Advert Project',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'db' => require(dirname(__DIR__) . '/config/db.php'),
+
+        'db' => require(dirname(__DIR__)."/config/db.php"),
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+//            'class' => 'yii\caching\FileCache',
+            'class' => 'common\cache\Base64Cache',
+        ],
+
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
         ],
     ],
 ];
