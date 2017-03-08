acab03c7b9ecc2d40d3d336f07e0896bfe0aae48
tetrostan <tetrostan@gmail.com>
2017-03-08 17:53:29 +0200

    Add images handlers actions to AdvertController: actionStep2(), actionFileUploadGeneral(), actionFileUploadImages().

--- frontend/modules/cabinet/controllers/AdvertController.php
+++ frontend/modules/cabinet/controllers/AdvertController.php
@@ -5,9 +5,17 @@ use common\controllers\AuthController;
 use Yii;
 use common\models\Advert;
 use common\models\Search\AdvertSearch;
-use yii\web\Controller;
+use yii\helpers\BaseFileHelper;
+use yii\helpers\Url;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
+use Imagine\Image\Point;
+use yii\imagine\Image;
+use Imagine\Gd;
+use Imagine\Image\Box;
+use \yii\helpers\FileHelper;
+use \yii\base\Exception;
 
 /**
  * AdvertController implements the CRUD actions for Advert model.
@@ -45,6 +53,65 @@ class AdvertController extends AuthController
         ]);
     }
 
+    public function actionFileUploadGeneral()
+    {
+        if (Yii::$app->request->isPost) {
+            $id = Yii::$app->request->post("advert_id");
+            $path = Yii::getAlias("@frontend/web/uploads/adverts/" . $id . "/general");
+            BaseFileHelper::createDirectory($path);
+            $model = Advert::findOne($id);
+            $model->scenario = 'step2';
+            //
+            $file = UploadedFile::getInstance($model, 'general_image');
+            $name = 'general.' . $file->extension;
+            $file->saveAs($path . DIRECTORY_SEPARATOR . $name);
+            //
+            $image = $path . DIRECTORY_SEPARATOR . $name;
+            $new_name = $path . DIRECTORY_SEPARATOR . "small_" . $name;
+            //
+            $model->general_image = $name;
+            $model->save();
+            //
+            $size = getimagesize($image);
+            $width = $size[0];
+            $height = $size[1];
+            Image::frame($image, 0, '666', 0)
+                ->crop(new Point(0, 0), new Box($width, $height))
+                ->resize(new Box(1000, 644))
+                ->save($new_name, ['quality' => 100]);
+        }
+
+        return true;
+    }
+
+    public function actionFileUploadImages()
+    {
+        if (Yii::$app->request->isPost) {
+            $id = Yii::$app->request->post("advert_id");
+            $path = Yii::getAlias("@frontend/web/uploads/adverts/" . $id);
+            BaseFileHelper::createDirectory($path);
+            $file = UploadedFile::getInstanceByName('images');
+            $name = time() . '.' . $file->extension;
+            $file->saveAs($path . DIRECTORY_SEPARATOR . $name);
+            //
+            $image = $path . DIRECTORY_SEPARATOR . $name;
+            $new_name = $path . DIRECTORY_SEPARATOR . "small_" . $name;
+            //
+            $size = getimagesize($image);
+            $width = $size[0];
+            $height = $size[1];
+            //
+            Image::frame($image, 0, '666', 0)
+                ->crop(new Point(0, 0), new Box($width, $height))
+                ->resize(new Box(1000, 644))
+                ->save($new_name, ['quality' => 100]);
+            //
+            sleep(1);
+        }
+
+        return true;
+    }
+
     /**
      * Creates a new Advert model.
      * If creation is successful, the browser will be redirected to the 'view' page.
@@ -54,7 +121,7 @@ class AdvertController extends AuthController
     {
         $model = new Advert();
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->idadvert]);
+            return $this->redirect(['step2']);
         } else {
             return $this->render('create', [
                 'model' => $model,
@@ -81,14 +148,42 @@ class AdvertController extends AuthController
             $data[] = $row->title;
         }*/
         // 2 variant
-        // $model = $this->findModel($id)->asArray;
+        // $model = $this->findModel($id)->asArray();
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->idadvert]);
+            return $this->redirect(['step2']);
         } else {
             return $this->render('update', [
                 'model' => $model,
             ]);
         }
+    }
+
+    public function actionStep2()
+    {
+        $id = Yii::$app->locator->cache->get('id');
+        $model = Advert::findOne($id);
+        $image = [];
+        if ($general_image = $model->general_image) {
+            $image[] = '<img src="/uploads/adverts/' . $model->idadvert . '/general/small_' . $general_image . '" width=250>';
+        }
+        if (Yii::$app->request->isPost) {
+            $this->redirect(Url::to(['advert/']));
+        }
+        $path = Yii::getAlias("@frontend/web/uploads/adverts/" . $model->idadvert);
+        $images_add = [];
+        try {
+            if (is_dir($path)) {
+                $files = FileHelper::findFiles($path);
+                foreach ($files as $file) {
+                    if (strstr($file, "small_") && !strstr($file, "general")) {
+                        $images_add[] = '<img src="/uploads/adverts/' . $model->idadvert . '/' . basename($file) . '" width=250>';
+                    }
+                }
+            }
+        } catch (Exception $e) {
+        }
+
+        return $this->render("step2", ['model' => $model, 'image' => $image, 'images_add' => $images_add]);
     }
 
     /**
@@ -122,5 +217,11 @@ class AdvertController extends AuthController
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
+    }
+
+    public function actionTest()
+    {
+        //
+        return $this->render("test");
     }
 }
