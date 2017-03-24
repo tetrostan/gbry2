63882a972531abec1d9aea0884c15c98090dae09
tetrostan <tetrostan@gmail.com>
2017-03-24 21:38:15 +0200

    Making links for each advert from home page. frontend/components/Common.php public static function getUrlAdvert($row) frontend/modules/main/views/default/index.php frontend/widgets/views/hot.php

--- frontend/components/Common.php
+++ frontend/components/Common.php
@@ -5,45 +5,41 @@ use yii\base\Component;
 use yii\helpers\BaseFileHelper;
 use yii\helpers\Url;
 
-class Common extends Component{
-
+class Common extends Component
+{
     const EVENT_NOTIFY = 'notify_admin';
 
-    public function sendMail($subject,$text,$emailFrom='optimus58@mail.ru',$nameFrom='Advert'){
-       /* if(\Yii::$app->mail->compose()
-            ->setFrom(['yii2.school@yandex.ru' => 'Advert'])
-            ->setTo([$emailFrom => $nameFrom])
+    public static function sendMail($email, $subject, $body, $name = '')
+    {
+        /*\Yii::$app->mail->compose()
+            ->setFrom([ \Yii::$app->params['supportEmail'] => \Yii::$app->name])
+            ->setTo([$email => $name])
             ->setSubject($subject)
-            ->setHtmlBody($text)
-            ->send()){
-            $this->trigger(self::EVENT_NOTIFY);
-            return true;
-        }*/
+            ->setTextBody($body)
+            ->send();*/
+//        $this->trigger(self::EVENT_NOTIFY); // ???
     }
 
-    public function notifyAdmin($event){
-
+    public function notifyAdmin($event)
+    {
         print "Notify Admin";
     }
 
-    public static function getTitleAdvert($data){
-
-        return $data['bedroom'].' Bed Rooms and '.$data['kitchen'].' Kitchen Room Aparment on Sale';
+    public static function getTitleAdvert($data)
+    {
+        return $data['bedroom'] . ' Bed Rooms and ' . $data['kitchen'] . ' Kitchen Room Apartment on Sale';
     }
 
-    public static function getImageAdvert($data,$general = true,$original = false){
-
+    public static function getImageAdvert($data, $general = true, $original = false)
+    {
         $image = [];
         $base = '/';
-        if($general){
-
-            $image[] = $base.'uploads/adverts/'.$data['idadvert'].'/general/small_'.$data['general_image'];
-        }
-        else{
-            $path = \Yii::getAlias("@frontend/web/uploads/adverts/".$data['idadvert']);
+        if ($general) {
+            $image[] = $base . 'uploads/adverts/' . $data['idadvert'] . '/general/small_' . $data['general_image'];
+        } else {
+            $path = \Yii::getAlias("@frontend/web/uploads/adverts/" . $data['idadvert']);
             $files = BaseFileHelper::findFiles($path);
-
-            foreach($files as $file){
+            foreach ($files as $file) {
                 if (strstr($file, "small_") && !strstr($file, "general")) {
                     $image[] = $base . 'uploads/adverts/' . $data['idadvert'] . '/' . basename($file);
                 }
@@ -53,16 +49,18 @@ class Common extends Component{
         return $image;
     }
 
-    public static function substr($text,$start=0,$end=50){
-
-        return mb_substr($text,$start,$end);
+    public static function substr($text, $start = 0, $end = 50)
+    {
+        return mb_substr($text, $start, $end);
     }
 
-
-    public static function getType($row){
+    public static function getType($row)
+    {
         return ($row['sold']) ? 'Sold' : 'New';
     }
 
-
-
+    public static function getUrlAdvert($row)
+    {
+        return Url::to(['/main/main/view-advert', 'id' => $row['idadvert']]);
+    }
 }
